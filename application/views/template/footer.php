8c969a0a4c66986a036e73479198e224af51847b
thiagodmm <thiagodmm@gmail.com>
2022-02-16 21:10:49 -0300

    Ajustes no Menu Responsivo e Data Tables

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,29 +1,29 @@
-        
-        <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
-        <script src="<?php echo base_url('assets/js/bootstrap.bundle.min.js'); ?>" crossorigin="anonymous"></script>
+</div>
 
-        <!-- Data Tables -->
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
-        <script type="text/javascript" src="https://cdn.datatables.net/v/bs5/jq-3.6.0/jszip-2.5.0/dt-1.11.4/af-2.3.7/b-2.2.2/b-html5-2.2.2/b-print-2.2.2/r-2.2.9/sb-1.3.1/sp-1.4.0/datatables.min.js"></script>
+<!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+<script src="<?php echo base_url('assets/js/bootstrap.bundle.min.js'); ?>" crossorigin="anonymous"></script>
 
-        <script>
-            $(document).ready(function() {
-            $('#lista').DataTable(
-                {
-                    language: {
-                        "url":"cdn.datatables.net/plug-ins/1.10.12/i18n/Portuguese-Brasil.json"
-                    },
-                    dom:'Bfrtip',
-                    buttons:[
-                        'pdf',
-                        'excel',
-                        'print'
-                    ]
-                }
-            );
-            } );
-        </script>
+<!-- Datatables -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
 
-    </body>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
+
+<script type="text/javascript" src="https://cdn.datatables.net/v/bs4/jq-3.6.0/jszip-2.5.0/dt-1.11.4/af-2.3.7/b-2.2.2/b-html5-2.2.2/b-print-2.2.2/r-2.2.9/sb-1.3.1/sp-1.4.0/datatables.min.js"></script>
+
+<script>
+  $(document).ready(function () {
+    $('#lista').DataTable({
+      language: {
+        "url": "https://cdn.datatables.net/plug-ins/1.10.12/i18n/Portuguese-Brasil.json"
+      },
+      dom: 'Bfrtip',
+      buttons: [
+        'pdf',
+        'excel',
+        'print'
+      ]
+    });
+  });
+</script>
+</body>
 </html>
--- application/views/template/header.php
+++ application/views/template/header.php
@@ -2,52 +2,51 @@
 <html lang="pt-br">
     <head>
         <!-- Required meta tags -->
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta charset="utf-8">
 
-        <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-        <!-- Estilos do Site -->
-        <link rel="stylesheet" href="<?php echo base_url('assets/css/estilos.css'); ?>">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css'); ?>">
 
-        <!-- Data Tables CSS -->
-        <link rel="stylesheet" href="<?php echo base_url('assets/datatables/datatables.min.css'); ?>">
- 
-        <!-- Data Tables JS -->
-        <script type="text/javascript" src="<?php echo base_url('assets/datatables/datatables.min.js'); ?>"></script>
+    <!-- Datatables -->
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/jq-3.6.0/jszip-2.5.0/dt-1.11.4/af-2.3.7/b-2.2.2/b-html5-2.2.2/b-print-2.2.2/r-2.2.9/sb-1.3.1/sp-1.4.0/datatables.min.css"/>
 
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <!-- Estilos do Site -->
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/estilos.css'); ?>">
 
-        <title>Projeto Crud PHP Codeigniter 3 | Bootstrap 5</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+    <title>Projeto Crud PHP Codeigniter 3 | Bootstrap 5</title>
     </head>
     <body>
         <div class="container">
             <?php if ($this->session->userdata('estou_logado')) { ?>
+                
                 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                    <div class="container-fluid">
-                        <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
-                            <span class="navbar-brand mb-0 h1"><?php echo $this->session->userdata('logado')->nomeUsuario; ?> </span>    
-                        </button>
-                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                                <span class="navbar-brand mb-0 h1"><?php echo $this->session->userdata('logado')->nomeUsuario; ?> </span>    
-                                <li class="nav-item">
-                                    <a class="nav-link" href="<?php echo base_url(); ?>login/sair">Sair</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="<?php echo base_url(); ?>pessoa">Pessoa</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="<?php echo base_url(); ?>usuario">Usuário</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="<?php echo base_url(); ?>carro">Carro</a>
-                                </li>
-                            </ul>
-                        </div>
+                <div class="container-fluid">
+                <span class="navbar-brand mb-0 h1"><?php echo $this->session->userdata('logado')->nomeUsuario; ?> </span>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                        <a class="nav-link" href="<?php echo base_url(); ?>login/sair">Sair</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="<?php echo base_url(); ?>pessoa">Pessoa</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="<?php echo base_url(); ?>usuario">Usuário</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="<?php echo base_url(); ?>carro">Carro</a>
+                        </li>
+                    </ul>
                     </div>
+                </div>
                 </nav>
+
             <?php } ?>
 
